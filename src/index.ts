51a229418143823f4629462cf110a975fc31b666
Manny Toribio <manny.mtoribio@gmail.com>
2021-10-29 11:00:06 -0400

    updated body parser

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import express from 'express'
 import cors from 'cors'
+import bodyParser from 'body-parser'
 import { teamsRouter } from './routes/team.routes'
 
 const app = express()
 app.use(cors())
+app.use(bodyParser())
 
 app.use('/teams', teamsRouter)
 
